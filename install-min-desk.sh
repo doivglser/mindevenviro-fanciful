9b0b09618604ac97dc1923f59935964a5192951e
Donko Ivo Glauser Serkovic <32725159+doivglser@users.noreply.github.com>
2017-10-23 00:52:52 -0500

    Update install-min-desk.sh

--- install-min-desk.sh
+++ install-min-desk.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-# version 0.01
+# version 1.00
 # first run installs the environment
 # run this script like: ../sh13lD/./inst4Ll
 # You can Add Programs to myPrograms, but remove bevor $HOME/installed
