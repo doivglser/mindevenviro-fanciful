c0ed4c1b7989a14de20330b701cfe2f41e21680d
Donko Ivo Glauser Serkovic <do.iv.gl.ser@gmx.es>
2017-11-13 01:52:31 -0500

    Update feh-bg.sh

--- feh-bg.sh
+++ feh-bg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # changes the background in vtwm, hold it from right-click
-feh --bg-scale "$(sort -R $HOME/Pictures/*.jpg | head -n1)" ;
+feh --bg-scale "$(sort -R "$HOME/Pictures/*.jpg" | head -n1)" ;
