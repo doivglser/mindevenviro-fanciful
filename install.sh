52a507583998af8a4ea4a587082b30928d12ed83
Donko Ivo Glauser Serkovic <do.iv.gl.ser@gmx.es>
2017-11-20 22:14:57 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ else
 		toInst=$(awk '{print $'"$nNuM"'}'<<<"$myPrograms") ;
 
 		wH3RE=$(whereis "$toInst" | cut -f2 -d: | cut -f2 -d\ ) ;
-# INSTALL
+		
 		if [[ "${#wH3RE}" != "0" ]] ;
 	then
 		apPR="$toInst, allready installed" ;
@@ -41,60 +41,57 @@ fi
 		echo -e "\n apt-get done." ;
 		sleep 3 ;
 fi
-# ON THE SCREEN
-		printf "\rtesting for apps, $nNuM remaining, $apPR" ; sleep 0.5 ;
-		clear ;
+		echo -e " ... testing for apps, $nNuM remaining, $apPR\n" ;
 done
-
-# ON THE SCREEN ::ENDE
-
-	# save vendors MAC-Address
+	echo -e "\n ... save vendors MAC-Address" ;
 	ip link show | grep ether | awk '{print $2}' | tee -a "/home/$SUDO_USER/vendorsmac" ;
 	chown "$SUDO_USER":"$SUDO_USER" "/home/$SUDO_USER/vendorsmac" ;
 	
-	# create $HOME directories
+	echo -e "\n ... create $HOME directories" ;
 	mkdir -p /home/$SUDO_USER/{testphp,testbash,testperl,testpython,Downloads,Pictures,Documents,Music,Ableton} 2>/dev/null ;
 	#mkdir -p /home/$SUDO_USER/.local/share/mc/skins/ 2>/dev/null ;
 	
-	# copy the .config/mc/ini to $HOME
+	echo -e "\n ... copy the .config/mc/ini to $HOME" ;
 	cp -f ini /home/$SUDO_USER/.config/mc/ini ;
 	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/.config/mc/ini ;
 	
-	# copy the .twmrc to $HOME
+	echo -e "\n ... copy the .twmrc to $HOME" ;
 	cp -f twmrc /home/$SUDO_USER/.twmrc ;
 	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/.twmrc ;
 	
-	# copy Xresources
+	echo -e "\n ... copy Xresources" ;
 	cp -f Xresources /etc/X11/xdm/ ;
 	chmod 755 /etc/X11/xdm/Xresources ;
 	
-	# copy VLC skin
+	echo -e "\n ... copy VLC skin" ;
 	cp -f 169311-inkyV2.vlt /usr/share/vlc/skins2/ ;
 	chmod 755 /usr/share/vlc/skins2/169311-inkyV2.vlt ;
 	
-	# copy the ~/.config/terminator/config to $HOME
+	echo -e "\n ... copy the ~/.config/terminator/config to $HOME" ;
 	cp -f config /home/$SUDO_USER/.config/terminator/config ;
 	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/.config/terminator/config ;
 	
-	# copy the .xinitrc to $HOME
+	echo -e "\n ... copy the .xinitrc to $HOME" ;
 	cp -f xinitrc /home/$SUDO_USER/.xinitrc ;
 	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/.xinitrc ;
 	
-	# copy the startup sound to $HOME/Music
+	echo -e "\n ... copy the startup sound to $HOME/Music" ;
 	cp -f 76256__ganscaile__startup.mp3 /home/$SUDO_USER/Music/ ;
 	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/Music/* ;
 	
-	# set aliases
+	echo -e "\n ... set aliases" ;
 	echo "alias ls='ls --color=auto -s'" >> /home/$SUDO_USER/.bashrc ;
 	
-	# set background Pictures
+	echo -e "\n ... set background Pictures" ;
 	unzip wallpapers.zip -d /home/$SUDO_USER/Pictures/ ;
 	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/Pictures/* ;
 	
-	# set the upload path for PHP scripts
+	echo -e "\n ... set the upload path for PHP scripts" ;
 	mkdir -p /var/www/testphp 2>/dev/null ;
 	echo "Alias /testphp/ /var/www/testphp/" >> /etc/apache2/sites-available/default ;
 		
+	echo -e "\n ...check for cgi-bin on drive" ;
+	
 		if [[ ! "$(cat /etc/apache2/sites-available/default | grep cgi-bin)" =~ '/usr/lib/cgi-bin/' ]] ;
 	then
 		mkdir -p /usr/lib/cgi-bin/ 2>/dev/null ;
@@ -108,18 +105,18 @@ done
 	else
 		echo "cgi-bin dir exists" ;
 fi
-		# update apache2 configuration
+		echo -e "\n ... update apache2 configuration" ;
 		a2dismod mpmevent && a2enmod mpmprefork && a2enmod cgid && wait ;
-		# restart server
+		echo -e "\n ... restart server" ;
 		systemctl restart apache2.service && wait ;
-		# set the rights for installed (to remove it later)
+		echo -e "\n ... set the rights for installed (to remove it later)" ;
 		chown "$SUDO_USER":"$SUDO_USER" "/home/$SUDO_USER/installed" ;
-	
+
+		echo -e "\n ...update scripts to path" ;
+		
 		while [[ "$nNuM2" != "0" ]] ;
 	do
-		clear ;
 		toCopyPath=$(awk '{print $'"$nNuM2"'}'<<<"$environinstall") ;
-		echo -e "\n" ;
 		sudo cp -f "$PWD/$toCopyPath" "/usr/local/bin/$toCopyPath" ;
 		sudo chown root:root "/usr/local/bin/$toCopyPath" ;
 		sudo chmod 4755 "/usr/local/bin/$toCopyPath" ;
