ac371a96c1007acd59b4c335c2f9e5dffca6b3ef
Donko Ivo Glauser Serkovic <do.iv.gl.ser@gmx.es>
2017-12-02 17:48:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,16 +13,45 @@ else
 		if [ ! -e "/home/$SUDO_USER/installed" ] && [[ $PWD =~ 'min-dev-enviro' ]] ;
 	then
 
-	nNuM=$(echo "$myPrograms" | wc -w) ;
-	nNuM2=$(echo "$environinstall" | wc -w) ;
+	nNuM1=$(echo "$environinstall" | wc -w) ;
+	nNuM2=$(echo "$myPrograms" | wc -w) ;
 
-		if [[ "$(df -h | grep -E /dev/run$ | cut -f2 -d% | tr -d '\ ')" != '' ]] ; 
+		if [[ "$(df -h | grep -E shm$ | cut -f2 -d% | tr -d '\ ')" != '' ]] ; 
 	then
-		tmpfolder="$(df -h | grep -E /run/shm$ | cut -f2 -d% | tr -d '\ ')" ;
+		tmpfolder="$(df -h | grep -E shm$ | cut -f2 -d% | tr -d '\ ')" ;
 	else
 		tmpfolder="/tmp" ;
 fi
-		while [[ "$nNuM" != "0" ]] ;
+	echo -e "\n ... BEGIN INSTALLATION ::" ;
+	
+		while [[ "$nNuM1" != "0" ]] ;
+	do
+		toCopyPath=$(awk '{print $'"$nNuM2"'}'<<<"$environinstall") ;
+		sudo cp -f "$PWD/$toCopyPath" "/usr/local/bin/$toCopyPath" ;
+		sudo chown root:root "/usr/local/bin/$toCopyPath" ;
+		sudo chmod 4755 "/usr/local/bin/$toCopyPath" ;
+		sudo chmod u+s "/usr/local/bin/$toCopyPath" ;
+		printf "\r$toCopyPath" ; sleep 0.5 ;
+		((nNuM1--)) ;
+done
+
+	echo -e "\n ... save vendors MAC-Address" ;
+	ip link show | grep ether | awk '{print $2}' | tee -a "/home/$SUDO_USER/vendorsmac" ;
+	chown "$SUDO_USER":"$SUDO_USER" "/home/$SUDO_USER/vendorsmac" ;
+	
+	echo -e "\n ... create /home/$SUDO_USER Directories" ;
+	mkdir -p /home/$SUDO_USER/{testphp,testbash,testperl,testpython,Downloads,Pictures,Documents,Music,Ableton} 2>/dev/null ;
+	chmod +t -R /home/$SUDO_USER/ ;
+	
+	echo -e "\n ... set background Pictures" ;
+	unzip wallpapers.zip -d /home/$SUDO_USER/Pictures/ && wait ;
+	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/Pictures/* ;
+	
+	echo -e "\n ... copy the startup sound to /home/$SUDO_USER/Music/" ;
+	cp -f 76256__ganscaile__startup.mp3 /home/$SUDO_USER/Music/ && wait ;
+	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/Music/* ;
+		
+		while [[ "$nNuM2" != "0" ]] ;
 	do
 		toInst=$(awk '{print $'"$nNuM"'}'<<<"$myPrograms") ;
 
@@ -31,7 +60,7 @@ fi
 		if [[ "${#wH3RE}" != "0" ]] ;
 	then
 		apPR="$toInst, allready installed" ;
-		((nNuM--)) ;
+		((nNuM2--)) ;
 	else
 		echo -e "\ninstalling ${toInst}..." ;
 		nohup apt-get -f -m -y install "$toInst" | tee -a "/home/$SUDO_USER/installed" &
@@ -39,76 +68,48 @@ fi
 		sleep 1 ;
 		apPR="$toInst, INSTALLED! " ;
 		sleep 0.5 ;
-		((nNuM--)) ;
+		((nNuM2--)) ;
 fi
-		if [ "$nNuM" = "0" ]
+		if [ "$nNuM2" = "0" ]
 	then
-		clear ;
 		echo -e "\n ... apt-get done." ;
-		sleep 3 ;
+		sleep 0.5 ;
 fi
 		printf "\r ... testing for apps, $nNuM remaining, $apPRn" ; sleep 0.5 ;
-		clear ;
 done
-	echo -e "\n ... copy scripts to /usr/local/bin" ;
-	
-		while [[ "$nNuM2" != "0" ]] ;
-	do
-		toCopyPath=$(awk '{print $'"$nNuM2"'}'<<<"$environinstall") ;
-		sudo cp -f "$PWD/$toCopyPath" "/usr/local/bin/$toCopyPath" ;
-		sudo chown root:root "/usr/local/bin/$toCopyPath" ;
-		sudo chmod 4755 "/usr/local/bin/$toCopyPath" ;
-		sudo chmod u+s "/usr/local/bin/$toCopyPath" ;
-		printf "\r$toCopyPath" ; sleep 0.5 ;
-		((nNuM2--)) ;
-done
-	echo -e "\n ... save vendors MAC-Address" ;
-	ip link show | grep ether | awk '{print $2}' | tee -a "/home/$SUDO_USER/vendorsmac" ;
-	chown "$SUDO_USER":"$SUDO_USER" "/home/$SUDO_USER/vendorsmac" ;
-	
-	echo -e "\n ... create $HOME directories" ;
-	mkdir -p /home/$SUDO_USER/{testphp,testbash,testperl,testpython,Downloads,Pictures,Documents,Music,Ableton} 2>/dev/null ;
-	#mkdir -p /home/$SUDO_USER/.local/share/mc/skins/ 2>/dev/null ;
-	
-	echo -e "\n ... copy the .config/mc/ini to $HOME" ;
-	cp -f ini /home/$SUDO_USER/.config/mc/ini ;
-	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/.config/mc/ini ;
-	
-	echo -e "\n ... copy the .twmrc to $HOME" ;
-	cp -f twmrc /home/$SUDO_USER/.twmrc ;
-	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/.twmrc ;
-	
+
 	echo -e "\n ... copy Xresources" ;
 	cp -f Xresources /etc/X11/xdm/ ;
 	chmod 755 /etc/X11/xdm/Xresources ;
 	
-	echo -e "\n ... copy VLC skin" ;
-	cp -f 169311-inkyV2.vlt /usr/share/vlc/skins2/ ;
-	chmod 755 /usr/share/vlc/skins2/169311-inkyV2.vlt ;
+	echo -e "\n ... update .bashrc" ;
+	echo "alias ls='ls --color=auto -s'" >> /home/$SUDO_USER/.bashrc ;
+	echo "set -o noclobber" >> /home/$SUDO_USER/.bashrc ;
 	
-	echo -e "\n ... copy the ~/.config/terminator/config to $HOME" ;
+	echo -e "\n ... copy the ini to /home/$SUDO_USER/.config/mc/" ;
+	cp -f ini /home/$SUDO_USER/.config/mc/ini ;
+	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/.config/mc/ini ;
+	
+	echo -e "\n ... copy the .twmrc to /home/$SUDO_USER" ;
+	cp -f twmrc /home/$SUDO_USER/.twmrc ;
+	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/.twmrc ;
+	
+	echo -e "\n ... copy the config to /home/$SUDO_USER/.config/terminator/" ;
 	cp -f config /home/$SUDO_USER/.config/terminator/config ;
 	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/.config/terminator/config ;
 	
-	echo -e "\n ... copy the bash_profile to $HOME" ;
+	echo -e "\n ... copy the bash_profile to /home/$SUDO_USER" ;
 	cp -f bash_profile /home/$SUDO_USER/.bash_profile ;
 	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/.bash_profile ;
 	
-	echo -e "\n ... copy the startup sound to $HOME/Music" ;
-	cp -f 76256__ganscaile__startup.mp3 /home/$SUDO_USER/Music/ ;
-	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/Music/* ;
-	
-	echo -e "\n ... set aliases in .bashrc" ;
-	echo "alias ls='ls --color=auto -s'" >> /home/$SUDO_USER/.bashrc ;
-	
-	echo -e "\n ... set background Pictures" ;
-	unzip wallpapers.zip -d /home/$SUDO_USER/Pictures/ ;
-	chown "$SUDO_USER":"$SUDO_USER" /home/$SUDO_USER/Pictures/* ;
-	
+	echo -e "\n ... copy VLC skin" ;
+	cp -f 169311-inkyV2.vlt /usr/share/vlc/skins2/ ;
+	chmod 755 /usr/share/vlc/skins2/169311-inkyV2.vlt ;
+
 	echo -e "\n ... set the upload path for PHP scripts" ;
 	mkdir -p /var/www/testphp 2>/dev/null ;
 	echo "Alias /testphp/ /var/www/testphp/" >> /etc/apache2/sites-available/default ;
-		
+	
 	echo -e "\n ...check for cgi-bin on drive" ;
 	
 		if [[ ! "$(cat /etc/apache2/sites-available/default | grep cgi-bin)" =~ '/usr/lib/cgi-bin/' ]] ;
@@ -125,9 +126,13 @@ done
 		echo -e "\n ... cgi-bin dir exists" ;
 fi
 		echo -e "\n ... update apache2 configuration" ;
-		a2dismod mpmevent && a2enmod mpmprefork && a2enmod cgid && wait ;
+		a2dismod mpmevent && wait ;
+		a2enmod mpmprefork && wait ;
+		a2enmod cgid && wait ;
+		
 		echo -e "\n ... restart server" ;
 		systemctl restart apache2.service && wait ;
+		
 		echo -e "\n ... set the rights for installed (to remove it later)" ;
 		chown "$SUDO_USER":"$SUDO_USER" "/home/$SUDO_USER/installed" ;
 
