e949f1f4f3de6230b9d1edac4874d15b60564fcd
doivglser <do.iv.gl.ser@gmx.es>
2018-01-25 20:00:37 +0100

    _ok

--- hi_vlc.sh
+++ hi_vlc.sh
@@ -2,7 +2,7 @@
 # startup file on /usr/local/bin
 
 
-if [[ $(ps axu | grep -v grep | grep -v hi_vlc.sh | grep STRYPER-VLC) = '' ]] ;
+if [[ $(ps axu | grep -v grep | grep -v hi_vlc.sh | grep 169311-inkyV2) = '' ]] ;
         then
                 vlc -I skins2 --skins2-last=/usr/share/vlc/skins2/169311-inkyV2.vlt file:///home/$USER/Music/ & hi.sh ;
         else
