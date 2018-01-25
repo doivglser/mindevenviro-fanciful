261e210f8d33403728ada7bfec6c6f4dfc8b90af
doivglser <do.iv.gl.ser@gmx.es>
2018-01-25 19:53:44 +0100

    Story: 4ppy

--- hi_vlc.sh
+++ hi_vlc.sh
@@ -4,7 +4,7 @@
 
 if [[ $(ps axu | grep -v grep | grep -v hi_vlc.sh | grep STRYPER-VLC) = '' ]] ;
         then
-                vlc -I skins2 --skins2-last=/usr/share/vlc/skins2/STRYPER-VLC.vlt & hi.sh ;
+                vlc -I skins2 --skins2-last=/usr/share/vlc/skins2/169311-inkyV2.vlt file:///home/$USER/Music/ & hi.sh ;
         else
                 exit 0 ;
 fi	
